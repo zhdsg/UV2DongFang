ed56c5e8e078a0ae0dd37352e45cc1f3b8500af5
zhdsg <704448732@qq.com>
2018-04-08 20:07:29 +0800

    更新20180408

--- src/main/scala/com/easyday/spark/UvAndIP2AnaylzeSpark.scala
+++ src/main/scala/com/easyday/spark/UvAndIP2AnaylzeSpark.scala
@@ -17,80 +17,99 @@ object UvAndIP2AnaylzeSpark {
 
   def main(args: Array[String]) {
 
-    val conf : SparkConf = new SparkConf()
+    val conf: SparkConf = new SparkConf()
       .setAppName(Constract.SPARK_APP_NAME)
-//      .setMaster("local")
-//      .set("spark.testing.memory","471859200")
-//      .set("spark.sql.warehouse.dir","file:////F://workspace//UV2DongFang")
-//      .set("spark.driver.memory","5g")
+      .set("spark.shuffle.consolidateFiles","true")
+    //      .setMaster("local")
+    //      .set("spark.testing.memory","471859200")
+    //      .set("spark.sql.warehouse.dir","file:////F://workspace//UV2DongFang")
+    //      .set("spark.driver.memory","5g")
 
-    val sc: SparkContext= SparkContext.getOrCreate(conf)
-   // sc.setCheckpointDir("/user/zhanghao/data/checkpoint")
-//    sc.setCheckpointDir("E:\\tool\\checkpoint")
-    val spark:SparkSession=SparkSession.builder()
-        .config(conf)
-        .enableHiveSupport()
-        .getOrCreate()
+    val sc: SparkContext = SparkContext.getOrCreate(conf)
+    // sc.setCheckpointDir("/user/zhanghao/data/checkpoint")
+    //    sc.setCheckpointDir("E:\\tool\\checkpoint")
+    val spark: SparkSession = SparkSession.builder()
+      .config(conf)
+      .enableHiveSupport()
+      .getOrCreate()
     import spark.implicits._
+    try {
+      //模拟数据
+      //mockData(spark)
 
+      val data1 = Nil
+      var tmpRDD: RDD[(String, Long)] = sc.parallelize(data1)
+      val zeroTime = DateUtil.getZeroTime(new Date()).getTime / 1000
+      val time_interval = new Date().getTime / 1000 - zeroTime
+      var i: Long = 0
+      for (i <- 1.toLong to time_interval / (ConfigurationManager.getInteger(Constract.TIME_INTERVAL) * 60)) {
 
-    mockData(spark)
-    val data1 =Nil
-    var tmpRDD :RDD[(String,Long)] =sc.parallelize(data1)
+        spark.sql(s"use ${ConfigurationManager.getString(Constract.HIVE_DATABASE)}")
+        spark.sql(s"set hive.input.format=org.apache.hadoop.hive.ql.io.CombineHiveInputFormat;")
+        val data: Dataset[Row] = spark.sql(s"select dateline,clientip,qid,uid from " +
+          //  s"${ConfigurationManager.getString(Constract.HIVE_DATABASE)}." +
+          s"${ConfigurationManager.getString(Constract.HIVE_TABLE)} " +
+          s"where dt ='${DateUtil.getTodayDate()}' " +
+          s"and dateline >='${zeroTime + (i - 1) * (ConfigurationManager.getInteger(Constract.TIME_INTERVAL) * 60)}' " +
+          s"and dateline < '${zeroTime + i * (ConfigurationManager.getInteger(Constract.TIME_INTERVAL) * 60)}' ")
+        data.show(20)
+        val dataRDD = data.rdd
 
-    val time_interval = new Date().getTime/1000 - DateUtil.getZeroTime(new Date()).getTime/1000
-    var i :Long =0
-    for(i <- 1.toLong to time_interval/(ConfigurationManager.getInteger(Constract.TIME_INTERVAL)*60)) {
-
-
-      val data: Dataset[Row] = spark.sql(s"select dateline,clientip,qid,uid from " +
-        s"${ConfigurationManager.getString(Constract.HIVE_DATABASE)}." +
-        s"${ConfigurationManager.getString(Constract.HIVE_TABLE)} " +
-        s"where dt ='${DateUtil.getTodayDate()}' and dateline >=''")
-      data.show(100)
-      val dataRDD = data.rdd
-
-      val uidRDDqid: RDD[(String, Long)] = dataRDD.map(
+        val uidRDDqid: RDD[(String, Long)] = dataRDD.map(
+          row => {
+            val key = s"${row.getString(2)}_${row.getString(3)}"
+            (key, 1.toLong)
+          }
+        )
+        val groupbyUidQidRDD: RDD[(String, Long)] = uidRDDqid.reduceByKey(_ + _)
+        groupbyUidQidRDD.persist(StorageLevel.MEMORY_ONLY)
+        tmpRDD = tmpRDD.union(groupbyUidQidRDD)
+        // tmpRDD.persist(StorageLevel.MEMORY_ONLY)
+      }
+      tmpRDD.persist(StorageLevel.MEMORY_ONLY)
+      //进行聚合
+      val key2QidRDD = tmpRDD.map(
         row => {
-          val key = s"${row.getString(2)}_${row.getString(3)}"
-          (key, 1.toLong)
+          val QidAndUid = row._1
+          val qid = QidAndUid.split("_")(0)
+         // val uid = QidAndUid.split("_")(1)
+          (qid, 1.toLong)
         }
       )
-      val groupbyUidQidRDD: RDD[(String, Long)] = uidRDDqid.reduceByKey(_ + _)
 
-       tmpRDD = tmpRDD.union(groupbyUidQidRDD)
-        tmpRDD.persist(StorageLevel.MEMORY_ONLY)
-    }
-
-    //进行聚合
-    val key2QidRDD =tmpRDD.map(
-      row =>{
-        val QidAndUid = row._1
-        val qid =QidAndUid.split("_")(0)
-        val uid =QidAndUid.split("_")(1)
-        (qid,1.toLong)
+      val groupBYqidRDD: RDD[(String, Long)] = key2QidRDD.reduceByKey(_ + _)
+      //    val resultRDD =groupBYqidRDD.map(
+      //      row => {
+      //        var sum = 0
+      //        val qid =row._1
+      //        val iterator:Iterator[String] = row._2.iterator
+      //        while (iterator.hasNext){
+      //          sum=sum+1
+      //        }
+      //        (qid,sum)
+      //      }
+      //
+      //    )
+      //    data.rdd.checkpoint()
+      //当前时间总UV
+      val key2UidRDD =tmpRDD.map(
+        row =>{
+          val QidAndUid = row._1
+         // val qid = QidAndUid.split("_")(0)
+          val uid = QidAndUid.split("_")(1)
+          (uid, 1.toLong)
+        }
+      )
+      val groupBYuidRDD: RDD[(String, Long)] = key2UidRDD.reduceByKey(_ + _)
+      groupBYqidRDD.collect().foreach(row => println(row._1 + "  : " + row._2))
+    }catch{
+      case e :Exception => {
+        throw new Exception(e)
       }
-    )
-
-    val  groupBYqidRDD:RDD[(String,Long)] = key2QidRDD.reduceByKey(_ + _)
-//    val resultRDD =groupBYqidRDD.map(
-//      row => {
-//        var sum = 0
-//        val qid =row._1
-//        val iterator:Iterator[String] = row._2.iterator
-//        while (iterator.hasNext){
-//          sum=sum+1
-//        }
-//        (qid,sum)
-//      }
-//
-//    )
-
-//    data.rdd.checkpoint()
-
-   // resultRDD.collect().foreach(row=>println(row._1 +":" +row._2))
-
-
+    }finally{
+      sc.stop()
+      spark.stop()
+    }
 
 
   }
--- src/main/scala/com/easyday/utils/MockData.scala
+++ src/main/scala/com/easyday/utils/MockData.scala
@@ -22,44 +22,50 @@ object MockData {
     val dt = DateUtil.getTodayDate()
     var i = 0
     val random = new Random()
-    for( i <- 1 to 10000){
+    val structType :StructType = StructType(
+      Array(StructField("dateline",DataTypes.StringType,true),
+        StructField("clientip",DataTypes.StringType,true),
+        StructField("qid",DataTypes.StringType,true),
+        StructField("uid",DataTypes.StringType,true),
+        StructField("dt",DataTypes.StringType,true))
+    )
+
+    for( i <- 1 to 100000000){
       val format =new SimpleDateFormat("yyyy-MM-dd 00:00:00")
-      val dateline:Long =format.parse(format.format(new Date())).getTime+random.nextInt(86400*1000)
+      val dateline:Long =DateUtil.getZeroTime(new Date()).getTime/1000+random.nextInt(86400)
       val qId = s"qudao${random.nextInt(255)}"
 
       val clientip =s"${random.nextInt(255)}.${random.nextInt(255)}.${random.nextInt(255)}.${random.nextInt(255)}"
-      val uId =UUID.randomUUID().toString.replace("-","")
+      val uId =s"${random.nextInt(9)}${random.nextInt(9)}${random.nextInt(9)}${random.nextInt(9)}"
 
 
       val row = RowFactory.create(String.valueOf(dateline),clientip,uId,qId,dt)
       rows.add(row)
+      if (i % 300000 ==0){
+
+        val df :Dataset[Row] =spark.createDataFrame(rows,structType)
+        spark.sql("use minieastdaywap")
+        df.registerTempTable("log_miniwap_active_realtime1")
+        spark.sql("insert into table log_miniwap_active_realtime_source partition(dt='20180408')" +
+          " select dateline, clientip,qid,uid from  log_miniwap_active_realtime1 ")
+        rows.clear()
+      }
     }
-
-    val structType :StructType = StructType(
-      Array(StructField("dateline",DataTypes.StringType,true),
-            StructField("clientip",DataTypes.StringType,true),
-            StructField("qid",DataTypes.StringType,true),
-            StructField("uid",DataTypes.StringType,true),
-            StructField("dt",DataTypes.StringType,true))
-    )
-
-    val df :Dataset[Row] =spark.createDataFrame(rows,structType)
-    df.registerTempTable("log_miniwap_active_realtime")
-    df.show(100)
 
   }
 
   def main(args: Array[String]) {
     val conf = new SparkConf()
       .setAppName("1")
-      .setMaster("local")
-      .set("spark.testing.memory","471859200")
-      .set("spark.sql.warehouse.dir","file:////F://workspace//UV2DongFang")
-      .set("spark.driver.memory","5g")
+      .set("spark.shuffle.consolidateFiles","true")
+//      .setMaster("local")
+//      .set("spark.testing.memory","471859200")
+//      .set("spark.sql.warehouse.dir","file:////F://workspace//UV2DongFang")
+//      .set("spark.driver.memory","5g")
 
 
     val sc =SparkContext.getOrCreate(conf)
-    val spark =SparkSession.builder().config(conf).getOrCreate()
+    val spark =SparkSession.builder().config(conf).enableHiveSupport().getOrCreate()
 
     mock(spark)
 
