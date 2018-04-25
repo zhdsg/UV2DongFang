2b672a91ed45e483989e52d099785d80cf3a4304
zhdsg <704448732@qq.com>
2018-04-25 14:37:22 +0800

    更新201804025

--- src/main/scala/com/easyday/constract/Constract.scala
+++ src/main/scala/com/easyday/constract/Constract.scala
@@ -7,6 +7,8 @@ object Constract {
 
   val SPARK_IS_LOCAL = "spark.islocal"
   val SPARK_APP_NAME = "UvAndIP2AnaylzeSpark"
+  val SPARK_APP_NAME_4_APP = "UvAndIP2AnaylzeSpark4APP"
+  val SPARK_APP_NAME_4_STREAMING = "PvAndUv4H5Streaming"
   val HIVE_DATABASE ="hive.database"
   val HIVE_TABLE="hive.table"
   val TIME_INTERVAL ="time.interval"
@@ -20,8 +22,10 @@ object Constract {
   val JDBC_URL ="jdbc.url";
   val JDBC_USER="jdbc.user";
   val JDBC_PASSWD="jdbc.passwd";
-  val TABLE_NAME="table.name"
-  val TABLE_NAME2="table.name2"
+  val H5_TABLE_NAME="h5.table.name"
+  val H5_TABLE_NAME2="h5.table.name2"
+  val APP_TABLE_NAME="app.table.name"
+  val APP_TABLE_NAME2="app.table.name2"
   //hadoop配置参数
   val DFS_CLIENT_SOCKET_TIMEOUT="dfs.client.socket-timeout"
   //spark配置参数
@@ -30,6 +34,8 @@ object Constract {
   val SPARK_REDUCER_MAXSIZEINFLIGHT="spark.reducer.maxSizeInFlight"
   val SPARK_SHUFFLE_IO_MAXRETRIES="spark.shuffle.io.maxRetries"
   val SPARK_DEFAULT_PARALLELISM="spark.default.parallelism"
-
+  //kafka信息
+  val KAFKA_TOPICS="kafka.topics"
+  val OFFSET_TABLE="offset.table"
 
 }
